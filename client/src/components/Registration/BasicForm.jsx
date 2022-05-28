ef64412282606ef02caba6322696304157ebe562
nastasia0305 <nastasia03@mail.ru>
2022-05-28 22:37:52 +0300

    города

--- client/src/components/Registration/BasicForm.jsx
+++ client/src/components/Registration/BasicForm.jsx
@@ -4,6 +4,24 @@ function BasicForm() {
   const { session } = useSelector(store => store.session)
   const { firstname, lastname, fathersname, email, city } = session
 
+  const cities = ['Москва', 'Санкт-Петербург', 'Новосибирск', 'Екатеринбург', 'Казань', 'Нижний Новгород', 'Челябинск', 'Омск', 'Самара', 'Ростов-на-Дону', 'Уфа']
+
+  // TODO: Перенести это в redux
+  // const getCities = () => {
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  //       'X-RapidAPI-Key': 'dec4b51b8emshafcc9f42e3d9f0ap1dc3ecjsn308dad847c27'
+  //     }
+  //   }
+    
+  //   return fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=500000&countryIds=ru&sort=-population&limit=10&languageCode=ru', options)
+  //   .then(response => response.json())
+  //   .then(({data}) => data.map(({city}) => city))
+  //   .catch(error => console.error(error))
+  // }
+
   return (
     <>
       <div className="row">
@@ -24,7 +42,13 @@ function BasicForm() {
       </div>
       <div className="row">
         <label htmlFor="city" className="label">Город</label>
-        <input type="text" className="input" name="city" placeholder="Город" defaultValue={city} required />
+        <select name="select" defaultValue={cities[0]} className="select">
+          {
+            cities.map((city, index) => {
+              return <option value={city} key={'city-' + index}>{city}</option>
+            })
+          }
+        </select>
       </div>
     </>
   )
