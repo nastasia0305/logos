33bc22ea2f717a76f0794ce3cfeaccdb5a23ae87
Evgen <sol9nkaaa@gmail.com>
2022-06-02 14:41:54 +0300

    admin_support_requests

--- server/routes/support.routes.js
+++ server/routes/support.routes.js
@@ -31,4 +31,23 @@ router.post('/support', async (req, res) => {
   }
 });
 
+router.get('/supports', async(req, res) => {
+  const getAllRequests = await Support.findAll()
+  console.log(getAllRequests);
+  res.status(200).json(getAllRequests)
+})
+
+router.put('/changestatus/:id', async (req, res) => {
+  const { id } = req.params
+  const changedStatus = await Support.update( { status: 'Выполнена' }, { where: { id } } )
+  res.status(200).json(id)
+})
+
+router.delete('/deletesupport/:id', async (req, res) => {
+  const { id } = req.params
+  const deletedSupport = await Support.destroy({ where: { id } })
+  console.log(id);
+  res.status(200).json(id)
+})
+
 module.exports = router;
