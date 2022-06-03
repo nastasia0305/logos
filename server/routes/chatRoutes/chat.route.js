6fb1eeb7d00ecb89c6140ac0f7044f880a8eb74d
Scream199109 <Scream199109@mail.ru>
2022-06-03 11:03:57 +0300

    fix lawyer

--- server/routes/chatRoutes/chat.route.js
+++ server/routes/chatRoutes/chat.route.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
-const { Request } = require('../../db/models')
+const { Request } = require('../../db/models');
 
 router.get('/allchats', async (req, res) => {
-  const allClientRequests = await Request.findAll({ where: { client_id: req.session.user.id}})
-  console.log(allClientRequests);
-  res.status(200).json(allClientRequests)
-})
+  let allRequests;
+  if (req.session.user.isLawyer) {
+    allRequests = await Request.findAll({ where: { lawyer_id: req.session.user.id } });
+  } else {
+    allRequests = await Request.findAll({ where: { client_id: req.session.user.id } });
+  }
+  res.status(200).json(allRequests);
+});
 
 router.get('/allOrders', async (req, res) => {
-  const allOrders = await Request.findAll()
+  const allOrders = await Request.findAll();
   console.log(allOrders);
-  res.status(200).json(allOrders)
-})
+  res.status(200).json(allOrders);
+});
 
 module.exports = router;
