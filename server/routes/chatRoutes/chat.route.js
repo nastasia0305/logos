769a180af326caf5c9ec3b5f008fb1ba93779e71
Evgen <sol9nkaaa@gmail.com>
2022-06-03 14:02:28 +0300

    admin_fix

--- server/routes/chatRoutes/chat.route.js
+++ server/routes/chatRoutes/chat.route.js
@@ -1,19 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { Request } = require('../../db/models');
+const { Request } = require("../../db/models");
+const { Op } = require("sequelize");
 
-router.get('/allchats', async (req, res) => {
+router.get("/allchats", async (req, res) => {
   let allRequests;
   if (req.session.user.isLawyer) {
-    allRequests = await Request.findAll({ where: { lawyer_id: req.session.user.id } });
+    allRequests = await Request.findAll({
+      where: { lawyer_id: req.session.user.id },
+    });
   } else {
-    allRequests = await Request.findAll({ where: { client_id: req.session.user.id } });
+    allRequests = await Request.findAll({
+      where: { client_id: req.session.user.id, lawyer_id: { [Op.not]: null } },
+    });
   }
   res.status(200).json(allRequests);
 });
 
-
-router.get('/allOrders', async (req, res) => {
+router.get("/allOrders", async (req, res) => {
   const allOrders = await Request.findAll();
   console.log(allOrders);
   res.status(200).json(allOrders);
