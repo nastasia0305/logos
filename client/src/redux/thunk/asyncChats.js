ab16992379c326eb6fa3f7630d797692aab48242
nastasia0305 <nastasia03@mail.ru>
2022-08-16 21:35:42 +0300

    init

--- client/src/redux/thunk/asyncChats.js
+++
@@ -1,9 +0,0 @@
-
-export const getChats = () => {
-  return (dispatch) => {
-    fetch('/allchats')
-      .then(response => response.json())
-      .then(data => dispatch({type: 'ALL_CHATS', payload: data}))
-      .catch(error => console.error(error))
-  }
-}
--- client/src/redux/thunk/news.js
+++
@@ -1,8 +0,0 @@
-export const getNews = () => {
-  return (dispatch) => {
-    fetch('/news')
-      .then(response => response.json())
-      .then(data => dispatch({ type: 'GET_NEWS', payload: data }))
-      .catch(error => console.error(error))
-  }
-}
